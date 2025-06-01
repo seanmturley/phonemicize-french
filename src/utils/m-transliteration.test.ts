510408e6b04ba5ed57d2db0a83946f0e75880a2a
Sean Turley <seanmturley@gmail.com>
2025-06-01 21:46:50 +1200

    [#54] Add elision tests for 'j', 'l', 'm', and 'n'

--- src/utils/m-transliteration.test.ts
+++ src/utils/m-transliteration.test.ts
@@ -4,6 +4,16 @@ describe("mTransliteration", () => {
   // NOTE: m as a marker of a nasal vowel is handled by vowel
   // transliteration functions.
 
+  it("m with elision should be /ʒ/", () => {
+    const word = "m'a";
+    const index = 0;
+
+    const [newTransliteration, newIndex] = mTransliteration(word, index);
+
+    expect(newTransliteration).toBe("m");
+    expect(newIndex).toBe(1);
+  });
+
   it("mm should be /m/", () => {
     const word = "flamme";
     const index = 3;
--- src/utils/n-transliteration.test.ts
+++ src/utils/n-transliteration.test.ts
@@ -4,6 +4,16 @@ describe("nTransliteration", () => {
   // NOTE: n as a marker of a nasal vowel is handled by vowel
   // transliteration functions.
 
+  it("n with elision should be /ʒ/", () => {
+    const word = "n'avait";
+    const index = 0;
+
+    const [newTransliteration, newIndex] = nTransliteration(word, index);
+
+    expect(newTransliteration).toBe("n");
+    expect(newIndex).toBe(1);
+  });
+
   it("nn should be /n/", () => {
     const word = "année";
     const index = 1;
