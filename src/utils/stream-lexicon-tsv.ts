b3faac37a1cc94958bebc0a435c052ec90f15754
Sean Turley <seanmturley@gmail.com>
2025-07-01 22:20:28 +1200

    [#63] Change to specific import of 'createReadStream' from the 'node:fs' module

--- src/utils/stream-lexicon-tsv.ts
+++ src/utils/stream-lexicon-tsv.ts
@@ -1,7 +1,7 @@
 import { parse } from "csv-parse";
-import * as fs from "node:fs";
+import { createReadStream } from "node:fs";
 
-fs.createReadStream("./src/lib/test-lexicon.txt")
+createReadStream("./src/lib/test-lexicon.txt")
   .pipe(parse({ delimiter: "\t", from_line: 3 }))
   .on("data", (row) => console.log(row))
   .on("end", () => console.log("Finished reading lexicon from file."))
