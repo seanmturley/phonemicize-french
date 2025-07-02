2557598a5b72a47e83c08a0fedfad60e9e02c964
Sean Turley <seanmturley@gmail.com>
2025-07-02 13:51:42 +1200

    [#65] Adapt TSV streaming utility to insert data in the database

--- src/utils/db/insert-lexicon-data.ts
+++ src/utils/db/insert-lexicon-data.ts
@@ -1,8 +1,16 @@
 import { parse } from "csv-parse";
 import { createReadStream } from "node:fs";
+import db from "./db.ts";
 
 createReadStream("./src/lib/test-lexicon.txt")
   .pipe(parse({ delimiter: "\t", from_line: 3 }))
-  .on("data", (row) => console.log(row))
+  .on("data", (row) => {
+    const insert = db.prepare(
+      "INSERT INTO lexicon (word, lemma, pos_tags) VALUES (?, ?, ?)"
+    );
+
+    const { lastInsertRowid } = insert.run(...row);
+    console.log(`Inserted a row with ID: ${lastInsertRowid}`);
+  })
   .on("end", () => console.log("Finished reading lexicon from file."))
   .on("error", (error) => console.log(error.message));
