0e87ea70bd3d756e1b4db5f922475da5624f4bf0
Sean Turley <seanmturley@gmail.com>
2025-07-04 18:45:41 +1200

    [#66] Update database INSERT code to provide primary and secondary POS tags

--- src/utils/db/insert-lexicon-data.ts
+++ src/utils/db/insert-lexicon-data.ts
@@ -1,15 +1,30 @@
 import { parse } from "csv-parse";
 import { createReadStream } from "node:fs";
 import db from "./db.ts";
+import formatPosTags from "../format-pos-tags.ts";
 
 createReadStream("./src/lib/test-lexicon.txt")
   .pipe(parse({ delimiter: "\t", from_line: 3 }))
   .on("data", (row) => {
     const insert = db.prepare(
-      "INSERT INTO lexicon (word, lemma, pos_tags) VALUES (?, ?, ?)"
+      `INSERT INTO lexicon (
+        word,
+        lemma,
+        primary_pos_tag,
+        secondary_pos_tags
+      )
+      VALUES (?, ?, ?, ?)`
     );
 
-    const { lastInsertRowid } = insert.run(...row);
+    const [primaryPosTag, secondaryPosTags] = formatPosTags(row[2], row[1]);
+
+    const { lastInsertRowid } = insert.run(
+      row[0],
+      row[1],
+      primaryPosTag,
+      secondaryPosTags
+    );
+
     console.log(`Inserted a row with ID: ${lastInsertRowid}`);
   })
   .on("end", () => console.log("Finished reading lexicon from file."))
