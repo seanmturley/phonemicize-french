cc93058a1c23081a54af86a9fb26c9e89cd41ab3
Sean Turley <seanmturley@gmail.com>
2025-07-10 21:47:21 +1200

    [#68] Add error handling to queryWord function

--- src/utils/db/query-word.ts
+++ src/utils/db/query-word.ts
@@ -1,11 +1,21 @@
 import db from "./db.ts";
 
 export default function queryWord(word: string) {
-  const query = db.prepare(`
-    SELECT *
-    FROM lexicon
-    WHERE word = ?
-    `);
+  try {
+    const query = db.prepare(`
+      SELECT *
+      FROM lexicon
+      WHERE word = ?
+      `);
 
-  return query.all(word);
+    return query.all(word);
+  } catch (error: unknown) {
+    if (error instanceof Error) {
+      console.error("Error:", error.message);
+    } else {
+      console.error("Error:", error);
+    }
+  } finally {
+    db.close();
+  }
 }
