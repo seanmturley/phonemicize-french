67e4301d9f9ecb32a473ddbcd189cca8a6bdf4f3
Sean Turley <seanmturley@gmail.com>
2025-07-21 18:05:31 +1200

    [#50] Flatten conjugation data structure

--- src/transliterate/verb-conjugation.test.ts
+++ src/transliterate/verb-conjugation.test.ts
@@ -1,118 +1,68 @@
 const premierGroupe = {
-  infinitif: "aimer",
-  participe: {
-    présent: "aimant",
-    passé: {
-      s: {
-        m: "aimé",
-        f: "aimée"
-      },
-      p: {
-        m: "aimés",
-        f: "aimées"
-      }
-    }
-  },
-  indicatif: {
-    présent: {
-      s: {
-        "1": "aime",
-        "2": "aimes",
-        "3": "aime"
-      },
-      p: {
-        "1": "aimons",
-        "2": "aimez",
-        "3": "aiment"
-      }
-    },
-    imparfait: {
-      s: {
-        "1": "aimais",
-        "2": "aimais",
-        "3": "aimait"
-      },
-      p: {
-        "1": "aimions",
-        "2": "aimiez",
-        "3": "aimaient"
-      }
-    },
-    futur: {
-      s: {
-        "1": "aimerai",
-        "2": "aimeras",
-        "3": "aimera"
-      },
-      p: {
-        "1": "aimerons",
-        "2": "aimerez",
-        "3": "aimeront"
-      }
-    },
-    "passé simple": {
-      s: {
-        "1": "aimai",
-        "2": "aimas",
-        "3": "aima"
-      },
-      p: {
-        "1": "aimâmes",
-        "2": "aimâtes",
-        "3": "aimèrent"
-      }
-    }
-  },
-  subjonctif: {
-    présent: {
-      s: {
-        "1": "aime",
-        "2": "aimes",
-        "3": "aime"
-      },
-      p: {
-        "1": "aimions",
-        "2": "aimiez",
-        "3": "aiment"
-      }
-    },
-    imparfait: {
-      s: {
-        "1": "aimasse",
-        "2": "aimasses",
-        "3": "aimât"
-      },
-      p: {
-        "1": "aimassions",
-        "2": "aimassiez",
-        "3": "aimassent"
-      }
-    }
-  },
-  conditionnel: {
-    présent: {
-      s: {
-        "1": "aimerais",
-        "2": "aimerais",
-        "3": "aimerait"
-      },
-      p: {
-        "1": "aimerions",
-        "2": "aimeriez",
-        "3": "aimeraient"
-      }
-    }
-  },
+  infinitif: { word: "aimer", ipa: "" },
 
-  impératif: {
-    présent: {
-      s: {
-        "2": "aime"
-      },
-      p: {
-        "1": "aimons",
-        "2": "aimez"
-      }
-    }
-  }
+  // Participe
+  ppr: { word: "aimant", ipa: "" },
+
+  "ppa m s": { word: "aimé", ipa: "" },
+  "ppa f s": { word: "aimée", ipa: "" },
+  "ppa m p": { word: "aimés", ipa: "" },
+  "ppa f p": { word: "aimées", ipa: "" },
+
+  // Indicatif
+  "ind pres 1 s": { word: "aime", ipa: "" },
+  "ind pres 2 s": { word: "aimes", ipa: "" },
+  "ind pres 3 s": { word: "aime", ipa: "" },
+  "ind pres 1 p": { word: "aimons", ipa: "" },
+  "ind pres 2 p": { word: "aimez", ipa: "" },
+  "ind pres 3 p": { word: "aiment", ipa: "" },
+
+  "ind impa 1 s": { word: "aimais", ipa: "" },
+  "ind impa 2 s": { word: "aimais", ipa: "" },
+  "ind impa 3 s": { word: "aimait", ipa: "" },
+  "ind impa 1 p": { word: "aimions", ipa: "" },
+  "ind impa 2 p": { word: "aimiez", ipa: "" },
+  "ind impa 3 p": { word: "aimaient", ipa: "" },
+
+  "ind futu 1 s": { word: "aimerai", ipa: "" },
+  "ind futu 2 s": { word: "aimeras", ipa: "" },
+  "ind futu 3 s": { word: "aimera", ipa: "" },
+  "ind futu 1 p": { word: "aimerons", ipa: "" },
+  "ind futu 2 p": { word: "aimerez", ipa: "" },
+  "ind futu 3 p": { word: "aimeront", ipa: "" },
+
+  "ind psim 1 s": { word: "aimai", ipa: "" },
+  "ind psim 2 s": { word: "aimas", ipa: "" },
+  "ind psim 3 s": { word: "aima", ipa: "" },
+  "ind psim 1 p": { word: "aimâmes", ipa: "" },
+  "ind psim 2 p": { word: "aimâtes", ipa: "" },
+  "ind psim 3 p": { word: "aimèrent", ipa: "" },
+
+  // Subjonctif
+  "sub pres 1 s": { word: "aime", ipa: "" },
+  "sub pres 2 s": { word: "aimes", ipa: "" },
+  "sub pres 3 s": { word: "aime", ipa: "" },
+  "sub pres 1 p": { word: "aimions", ipa: "" },
+  "sub pres 2 p": { word: "aimiez", ipa: "" },
+  "sub pres 3 p": { word: "aiment", ipa: "" },
+
+  "sub impa 1 s": { word: "aimasse", ipa: "" },
+  "sub impa 2 s": { word: "aimasses", ipa: "" },
+  "sub impa 3 s": { word: "aimât", ipa: "" },
+  "sub impa 1 p": { word: "aimassions", ipa: "" },
+  "sub impa 2 p": { word: "aimassiez", ipa: "" },
+  "sub impa 3 p": { word: "aimassent", ipa: "" },
+
+  // Conditionnel
+  "cond pres 1 s": { word: "aimerais", ipa: "" },
+  "cond pres 2 s": { word: "aimerais", ipa: "" },
+  "cond pres 3 s": { word: "aimerait", ipa: "" },
+  "cond pres 1 p": { word: "aimerions", ipa: "" },
+  "cond pres 2 p": { word: "aimeriez", ipa: "" },
+  "cond pres 3 p": { word: "aimeraient", ipa: "" },
+
+  // Impératif
+  "imp pres 2 s": { word: "aime", ipa: "" },
+  "imp pres 1 p": { word: "aimons", ipa: "" },
+  "imp pres 2 p": { word: "aimez", ipa: "" }
 };
