eb4a94d48bc63f98f615acecaf5536e90febc83e
Sean Turley <seanmturley@gmail.com>
2025-07-21 17:24:25 +1200

    [#50] Add verb conjugations for the '1e groupe'

---
+++ src/transliterate/verb-conjugation.test.ts
@@ -0,0 +1,118 @@
+const premierGroupe = {
+  infinitif: "aimer",
+  participe: {
+    présent: "aimant",
+    passé: {
+      s: {
+        m: "aimé",
+        f: "aimée"
+      },
+      p: {
+        m: "aimés",
+        f: "aimées"
+      }
+    }
+  },
+  indicatif: {
+    présent: {
+      s: {
+        "1": "aime",
+        "2": "aimes",
+        "3": "aime"
+      },
+      p: {
+        "1": "aimons",
+        "2": "aimez",
+        "3": "aiment"
+      }
+    },
+    imparfait: {
+      s: {
+        "1": "aimais",
+        "2": "aimais",
+        "3": "aimait"
+      },
+      p: {
+        "1": "aimions",
+        "2": "aimiez",
+        "3": "aimaient"
+      }
+    },
+    futur: {
+      s: {
+        "1": "aimerai",
+        "2": "aimeras",
+        "3": "aimera"
+      },
+      p: {
+        "1": "aimerons",
+        "2": "aimerez",
+        "3": "aimeront"
+      }
+    },
+    "passé simple": {
+      s: {
+        "1": "aimai",
+        "2": "aimas",
+        "3": "aima"
+      },
+      p: {
+        "1": "aimâmes",
+        "2": "aimâtes",
+        "3": "aimèrent"
+      }
+    }
+  },
+  subjonctif: {
+    présent: {
+      s: {
+        "1": "aime",
+        "2": "aimes",
+        "3": "aime"
+      },
+      p: {
+        "1": "aimions",
+        "2": "aimiez",
+        "3": "aiment"
+      }
+    },
+    imparfait: {
+      s: {
+        "1": "aimasse",
+        "2": "aimasses",
+        "3": "aimât"
+      },
+      p: {
+        "1": "aimassions",
+        "2": "aimassiez",
+        "3": "aimassent"
+      }
+    }
+  },
+  conditionnel: {
+    présent: {
+      s: {
+        "1": "aimerais",
+        "2": "aimerais",
+        "3": "aimerait"
+      },
+      p: {
+        "1": "aimerions",
+        "2": "aimeriez",
+        "3": "aimeraient"
+      }
+    }
+  },
+
+  impératif: {
+    présent: {
+      s: {
+        "2": "aime"
+      },
+      p: {
+        "1": "aimons",
+        "2": "aimez"
+      }
+    }
+  }
+};
