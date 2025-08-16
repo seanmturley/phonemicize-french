ef502bc7478f2b9e63446f0216952e9edb50fadb
Sean Turley <seanmturley@gmail.com>
2025-08-16 21:14:19 +1200

    [#75] Add tests and transliteration rules for proper and singular nouns with final 'd'

--- src/transliterate/letter/d.test.ts
+++ src/transliterate/letter/d.test.ts
@@ -1,14 +1,49 @@
 import dTransliteration from "./d.ts";
 
 describe("dTransliteration", () => {
-  it("final d should be /t/ in liaison, but otherwise silent", () => {
-    const word = "grand";
-    const index = 4;
+  describe("final d", () => {
+    it("should be /t/ in liaison, but otherwise silent", () => {
+      const word = "grand";
+      const index = 4;
 
-    const [newTransliteration, newIndex] = dTransliteration({ word, index });
+      const [newTransliteration, newIndex] = dTransliteration({
+        word,
+        index
+      });
 
-    expect(newTransliteration).toBe("(t)");
-    expect(newIndex).toBe(5);
+      expect(newTransliteration).toBe("(t)");
+      expect(newIndex).toBe(5);
+    });
+
+    it("should be silent for proper nouns", () => {
+      const word = "Arnaud";
+      const posArray = ["nom propre"];
+      const index = 5;
+
+      const [newTransliteration, newIndex] = dTransliteration({
+        word,
+        posArray,
+        index
+      });
+
+      expect(newTransliteration).toBe("");
+      expect(newIndex).toBe(6);
+    });
+
+    it("should be silent for singular nouns", () => {
+      const word = "blond";
+      const posArray = ["nom commun", "singulier"];
+      const index = 4;
+
+      const [newTransliteration, newIndex] = dTransliteration({
+        word,
+        posArray,
+        index
+      });
+
+      expect(newTransliteration).toBe("");
+      expect(newIndex).toBe(5);
+    });
   });
 
   describe("initial or medial", () => {
--- src/transliterate/letter/d.ts
+++ src/transliterate/letter/d.ts
@@ -5,6 +5,7 @@ import type {
 
 export default function dTransliteration({
   word,
+  posArray,
   index
 }: LetterTransliterationArgs): LetterTransliteration {
   const remainingWord = word.substring(index);
@@ -12,8 +13,18 @@ export default function dTransliteration({
   let newTransliteration = "";
   let numTransliteratedCharacters = 1;
 
-  if (/^d$/i.test(remainingWord)) {
-    // final d should be /t/ in liaison, but otherwise silent
+  if (/^d$/i.test(remainingWord) && posArray?.includes("nom propre")) {
+    // final d should be silent for proper nouns
+    newTransliteration = "";
+  } else if (
+    /^d$/i.test(remainingWord) &&
+    posArray?.includes("nom commun") &&
+    posArray?.includes("singulier")
+  ) {
+    // final d should be silent for singular nouns
+    newTransliteration = "";
+  } else if (/^d$/i.test(remainingWord)) {
+    // otherwise final d should be /t/ in liaison, but otherwise silent
     newTransliteration = "(t)";
   } else if (/^dd/i.test(remainingWord)) {
     // initial or medial dd should be /d/
