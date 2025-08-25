fcb008a0d13b68de816b77f10cdb0f1463e0a0e5
Sean Turley <seanmturley@gmail.com>
2025-08-25 16:26:00 +1200

    [#22] Remove redundant test for 'l with elision'

--- src/transliterate/letter/l.test.ts
+++ src/transliterate/letter/l.test.ts
@@ -3,16 +3,6 @@ import lTransliteration from "./l.ts";
 describe("lTransliteration", () => {
   // NOTE: There are several graphemes with il* that are handled by
   // the iTransliteration function.
-  it("l with elision should be /ʒ/", () => {
-    const word = "l'homme";
-    const index = 0;
-
-    const [newTransliteration, newIndex] = lTransliteration({ word, index });
-
-    expect(newTransliteration).toBe("l");
-    expect(newIndex).toBe(1);
-  });
-
   it("ll should be /l/", () => {
     const word = "ballet";
     const index = 2;
